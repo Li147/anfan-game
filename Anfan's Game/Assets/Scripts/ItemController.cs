0b96046a5294208becb11fa9cad935f2a3a9f8a6
Anfan <anfanli@gmail.com>
2019-07-13 01:35:19 +0100

    tried implementing pickupable items
    difficulties getting UI to update after item has been picked up

--- Anfan's Game/Assets/Scripts/ItemController.cs
+++ Anfan's Game/Assets/Scripts/ItemController.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Pickup : MonoBehaviour {
+public class ItemController : MonoBehaviour {
+
+    [SerializeField]
+    private Sprite[] itemSprites;
+
+    [SerializeField]
+    private int itemID;
 
     private Inventory inventory;
     public GameObject itemButton;
-
+   
     
 
-
     private void Start() {
-
+        
+        
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
-
-
 
     }
 
@@ -44,7 +49,11 @@ public class Pickup : MonoBehaviour {
                 if (inventory.isFull[i] == false) {
 
                     inventory.isFull[i] = true;
-                    Instantiate(itemButton, inventory.slots[i].transform, false);
+
+                    GameObject button = (GameObject) Instantiate(itemButton, inventory.slots[i].transform, false);
+                    //button.GetComponent<Image>().sprite = itemSprites[1];
+
+                    itemButton.GetComponent<Image>().sprite = itemSprites[itemID];
                     
 
                     Destroy(gameObject);
