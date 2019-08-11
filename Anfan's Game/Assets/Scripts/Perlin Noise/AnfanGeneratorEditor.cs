90c3be050907308be339fefac1065194b8defc36
Anfan <anfanli@gmail.com>
2019-08-11 01:29:31 +0100

    map generation
    -implemented basic perlin noise map generation
    -implemented custom tool in editor to more easily generate maps without having to start the game
    -generated maps with grass, water, rock, sand tiles

---
+++ Anfan's Game/Assets/Scripts/Perlin Noise/AnfanGeneratorEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AnfanTileMap))]
+public class AnfanGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        AnfanTileMap mapGen = (AnfanTileMap)target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.DisplayMapGUI();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.DisplayMapGUI();
+        }
+    }
+}
---
+++ Anfan's Game/Assets/Scripts/Perlin Noise/AnfanTileMap.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class AnfanTileMap : MonoBehaviour
+{
+    private float[,] terrainNoiseMap;
+    public Tilemap tileMap;
+    public Tilemap treeMap;
+
+    public int mapWidth;
+    public int mapHeight;
+    public int seed;
+    public float noiseScale;
+
+    [Range(0, 10)]
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+
+    public Vector2 offset;
+    public bool autoUpdate;
+
+    public TileType[] tileTypes;
+
+
+    public void DisplayMap()
+    {
+        terrainNoiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, 
+            persistance, lacunarity, offset);
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                float currentHeight = terrainNoiseMap[x, y];
+
+                for (int i = 0; i < tileTypes.Length; i++)
+                {
+                    if (currentHeight <= tileTypes[i].height)
+                    {
+                        tileMap.SetTile(new Vector3Int(x, y, 0), tileTypes[i].tile);
+                        break;
+
+                        
+                    }
+                }
+
+            }
+        }
+    }
+
+    public void DisplayMapGUI()
+    {
+        terrainNoiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, 1234567, 75, 3, 0.44f, 1, Vector2.zero);
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                float currentHeight = terrainNoiseMap[x, y];
+
+                for (int i = 0; i < tileTypes.Length; i++)
+                {
+                    if (currentHeight <= tileTypes[i].height)
+                    {
+                        //tileMap.SetTile(new Vector3Int(x, y, 0), tileTypes[i].tile);
+                        tileMap.SetEditorPreviewTile(new Vector3Int(x, y, 0), tileTypes[i].tile);
+                        break;
+
+
+                    }
+                }
+
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+    public void ClearMap()
+    {
+        tileMap.ClearAllTiles();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DisplayMap();
+        }
+
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ClearMap();
+        }
+    }
+
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+
+        if (lacunarity < 1)
+        {
+            lacunarity = 1; ;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0; ; ;
+        }
+    }
+
+
+
+
+
+}
+
+[System.Serializable]
+public struct TileType
+{
+    public string name;
+    public float height;
+    public Tile tile;
+}
